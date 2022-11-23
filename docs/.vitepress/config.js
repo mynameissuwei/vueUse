0147a030aa8e832083587280043b80dbf44fae20
mynameissuwei <910933955@qq.com>
2022-11-23 13:36:50 +0800

    fix: 修改base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,6 @@
 export default {
     title: 'VitePress',
     description: 'Just playing around.',
-    base:'https://mynameissuwei.github.io/'
+    base:'https://mynameissuwei.github.io/bar'
 }
   
---
+++ readme.md
@@ -0,0 +1,3 @@
+git config --global user.email "910933955@qq.com"
+git config --global user.name "mynameissuwei"
+git config --list 
