567b126a30e42b75be1ed61dc3d0efd8dccd69e0
mynameissuwei <910933955@qq.com>
2022-11-23 17:59:17 +0800

    fix: bug

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,19 @@
 export default {
+    themeConfig: {
+      sidebar: [
+        {
+          text: 'Guide',
+          items: [
+            { text: 'Introduction', link: '/introduction' },
+            { text: 'Getting Started', link: '/getting-started' },
+          ]
+        }
+      ]
+    },
     title: 'VitePress',
     description: 'Just playing around.',
-    base:'https://mynameissuwei.github.io/bar'
-}
-  
+    base:'https://mynameissuwei.github.io/bar',
+  }
+
+
+
